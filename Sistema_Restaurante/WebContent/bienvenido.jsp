5e9a8b0986414aff6683537a63ec5d3049162da6
Colo1396 <nazarenogalvan@outlook.com>
2017-05-25 18:56:11 -0300

    Agrego Empleados
    Se agrega los archivos de EmpleadoDAO, EmpleadoABM  y  html de la pagina
    de inicio de sesion.
    Hay un problema al hacer el test mediante consola y mediante html porque
    dice que hay un error en el mapeo... se probo el hql en Mysql y funciona
    la consulta

---
+++ Sistema_Restaurante/WebContent/bienvenido.jsp
@@ -0,0 +1,22 @@
+<%@ page language = "java" contentType = "text/html; charset=ISO-8859-1"
+pageEncoding = "ISO-8859-1" %>
+<%@ page import = "datos.Empleado" %>
+<! DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd" >
+<html >
+<head >
+<meta http-equiv = "Content-Type" content = "text/html; charset=ISO-8859-1" >
+<title >Sistema_Restaurante</title >
+</head >
+<BODY >
+<%@ include file = "/cabecera.jsp" %>
+<% Empleado empleado=(Empleado)request.getAttribute( "empleado" ); %>
+<BR >
+
+Bienvenido señor: <%= empleado.getNombreUsuario() %> <BR >
+
+<BR >
+<A href = "/Sistema_Restaurante.v.4/index.jsp" > Volver... </A >
+</BODY >
+</html >
+
---
+++ Sistema_Restaurante/src/dao/EmpleadoDao.java
@@ -0,0 +1,101 @@
+package dao;
+
+import java.util.List;
+
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+
+//import datos.Cliente;
+import datos.Persona;
+import datos.Empleado;
+
+
+public class EmpleadoDao {
+	private static Session session ;
+	private Transaction tx ;
+	
+	private void iniciaOperacion() throws HibernateException {
+		session = HibernateUtil. getSessionFactory ().openSession();
+		tx = session .beginTransaction();
+	}
+	
+	private void manejaExcepcion(HibernateException he) throws HibernateException {
+		tx .rollback();
+		throw new HibernateException( "ERROR en la capa de acceso a datos" , he);
+	}
+	
+	public Empleado traerEmpleado(long idEmpleado ) throws HibernateException
+	{
+		Empleado objeto = null;
+		try {
+			iniciaOperacion();
+			objeto = (Empleado) session.get(Empleado.class, idEmpleado);
+			//Hibernate.initialize(objeto.getRubro());
+			//Hibernate.initialize(objeto.getSubrubro());
+		}finally {
+			session.close();
+		}
+		return objeto;
+		
+	}
+	
+	public Empleado traerEmpleado(int dni) throws HibernateException
+	{
+		Empleado objeto = null;
+		try {
+			iniciaOperacion();
+			objeto = (Empleado) session .createQuery( "from Empleado e where e.dni=" +dni).uniqueResult();
+			//Hibernate.initialize(objeto.getRubro());
+			//Hibernate.initialize(objeto.getSubrubro());
+		}finally {
+			session.close();
+		}
+		return objeto;
+		
+	}
+	
+	/*public Empleado iniciarSesion( String email,String clave) throws HibernateException {
+		Empleado objeto = null ;
+		try {
+		iniciaOperacion();
+		objeto = (Empleado) session.createQuery( "from Empleado e where e.email='"+email+"'and e.clave='"+clave+"' and e.esActivo=1").uniqueResult();
+		} finally {
+		session.close();
+		}
+		return objeto;
+		}*/
+	public Empleado iniciarSesion( String usuario,String clave) throws HibernateException {
+		Empleado objeto = null ;
+		try {
+		iniciaOperacion();
+		objeto = (Empleado) session.createQuery( "from Empleado e where e.nombreUsuario='"+usuario+"'and e.clave='"+clave+"'").uniqueResult();
+		} finally {
+		session.close();
+		}
+		return objeto;
+		}
+	
+	@SuppressWarnings("unchecked")
+	public List<Empleado> traerEmpleado() throws HibernateException
+	{
+		List<Empleado> lista = null;
+		try {
+			iniciaOperacion();
+			String hql = "from Empleado u ";	
+			lista = session.createQuery(hql).list();
+			/*for (Usuario u : lista)
+			{
+				Hibernate.initialize(p.getRubro());
+				Hibernate.initialize(p.getSubrubro());
+			}*/
+		}finally {
+			session.close();
+		}
+		return lista;
+		
+	}
+	
+}
