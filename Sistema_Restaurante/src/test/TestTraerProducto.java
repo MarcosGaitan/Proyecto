1fca9d9b8de25fff620683bb75ee1faa8afb719a
MarcosGaitan <marcos_gaitan_1@outlook.com>
2017-06-13 19:02:34 -0300

    Ordenar Listas
    - esta todo en el Test.. es la funcion Collections.sort q encierra todo
    de una.

--- Sistema_Restaurante/src/test/TestTraerProducto.java
+++ Sistema_Restaurante/src/test/TestTraerProducto.java
@@ -1,5 +1,9 @@
 package test;
 
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import datos.Producto;
@@ -13,7 +17,7 @@ public class TestTraerProducto {
 		Producto p = null;
 		List<Producto> lista = null;
 
-		
+		/*
 		try {
 			long idProducto= 1;
 			p = abm.traerProducto(idProducto);
@@ -21,20 +25,37 @@ public class TestTraerProducto {
 		}catch(Exception e){
 			System.out.println(e.getMessage());
 		}
+		*/
 		
-		/*
 		try 
 		{
 			lista = abm.traerProducto();
+			
+			Collections.sort(lista, new Comparator<Producto>() {
+				
+				public int compare(Producto o1, Producto o2) {
+					
+					if (o1.getNombre().compareToIgnoreCase(o2.getNombre()) < 0){
+						
+						return -1;
+					}
+					else{
+						
+						return 0;
+					}
+				}
+
+			});
+
 			for (Producto k : lista)
 			{
-				System.out.println("\n "+ k);
+				System.out.println("\n "+ k.getNombre());
 			}
 		}catch(Exception e1)
 		{
 			System.out.println(e1.getMessage());
 		}
-		*/
+		
 	}
 
 }
